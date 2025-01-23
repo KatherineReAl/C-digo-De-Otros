8463d538c08191860d52624958cdd5f884a794f8
Katherine RA <kathephone1805@gmail.com>
2025-01-23 11:13:10 -0600

    Agregado algunas correcciones del ejercicio código-otros-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $b = document.querySelector('blog');//se pone como en el html #blo a blog
+const $l = document.querySelector('location');//le quiete el punto para que coincida como lo puse en el index
 
-function displayUser(username) {
+async function displayUser(username) { //puse la función con async para poder cachar bien el await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
